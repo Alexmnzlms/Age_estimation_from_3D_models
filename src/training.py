e027a39b745ef7360181d5f4583c273867ee1981
Alejandro Manzanares <alexmnzlms@gmail.com>
2024-01-11 15:25:14 +0100

    Training new structure and argparser

--- src/training.py
+++ src/training.py
@@ -31,7 +31,6 @@ import os
 import cv2 as cv
 
 import random
-
 import ranges_of_age as roa
 
 import sys
@@ -40,243 +39,338 @@ from datetime import datetime
 
 import functions as fn
 
-################################################################################
-BATCH_SIZE = 32
-EPOCHS = 100
-REGRESSION = True
-COLORMODE = sys.argv[1]
-VERBOSE = 2
-RESNET = bool(int(sys.argv[2]))
-SEED = int(sys.argv[3])
-OUTPUT = sys.argv[4]
+import argparse
+import logging
 
-if COLORMODE == 'rgb':
-    IMG_SHAPE = (36,108)
-    depth = 3
-    if RESNET:
-        cnn = fn.create_CNN_Resnet
-    else:
-        cnn = fn.create_CNN
-elif COLORMODE == 'grayscale':
-    IMG_SHAPE = (108,108)
-    depth = 1
-    cnn = fn.create_CNN
+import json
 
-mse = []
-r2 = []
-rmse = []
-mae = []
-fold = 1
-
-random.seed(SEED)
-np.random.seed(SEED)
-tf.random.set_seed(SEED)
 
 ################################################################################
-cnn_0 = cnn(0,IMG_SHAPE[0],IMG_SHAPE[1],depth)
-cnn_1 = cnn(1,IMG_SHAPE[0],IMG_SHAPE[1],depth)
-cnn_2 = cnn(2,IMG_SHAPE[0],IMG_SHAPE[1],depth)
 
-x = Average()([cnn_0.output, cnn_1.output, cnn_2.output])
+def main(train_path, validation_path, test_path, data_path, checkpoint_filepath, 
+         batch_size, color_mode, epochs, output, resnet, seed, img_shape, depth, 
+         cnn, verbose_level, pretrained_model):
 
-model = Model(inputs=[cnn_0.input, cnn_1.input, cnn_2.input], outputs=x)
+    mse = []
+    r2 = []
+    rmse = []
+    mae = []
+    name = []
+    fold = 1
+    ################################################################################
+    cnn_0 = cnn(0,img_shape[0],img_shape[1],depth)
+    cnn_1 = cnn(1,img_shape[0],img_shape[1],depth)
+    cnn_2 = cnn(2,img_shape[0],img_shape[1],depth)
 
-optimizer = Adam()
-
-checkpoint_filepath = './checkpoint/'
-
-model.compile(loss=MeanAbsoluteError(), optimizer=optimizer, metrics=['mae'])
-model.summary()
-
-weights = model.get_weights()
-
-if RESNET:
-    print('FINE TUNNING')
-    cnn_0 = fn.create_CNN_Resnet(0,IMG_SHAPE[0],IMG_SHAPE[1],depth,True)
-    cnn_1 = fn.create_CNN_Resnet(1,IMG_SHAPE[0],IMG_SHAPE[1],depth,True)
-    cnn_2 = fn.create_CNN_Resnet(2,IMG_SHAPE[0],IMG_SHAPE[1],depth,True)
     x = Average()([cnn_0.output, cnn_1.output, cnn_2.output])
 
-    model_ft = Model(inputs=[cnn_0.input, cnn_1.input, cnn_2.input], outputs=x)
+    model = Model(inputs=[cnn_0.input, cnn_1.input, cnn_2.input], outputs=x)
 
-    optimizer_ft = Adam(1e-5)
+    optimizer = Adam()
 
-    model_ft.compile(loss=MeanAbsoluteError(), optimizer=optimizer_ft, metrics=['mae'])
-    model_ft.summary()
+    model.compile(loss=MeanAbsoluteError(), optimizer=optimizer, metrics=['mae'])
+    model.summary()
 
-################################################################################
+    weights = model.get_weights()
 
-print('Data:', COLORMODE)
-print('Batch:', BATCH_SIZE)
-print('Epochs:', EPOCHS)
-print('Seed:', SEED)
-if RESNET:
-    print('RESNET50')
-else:
-    print('PANORAMA-CNN')
+    if resnet:
+        print('FINE TUNNING')
+        cnn_0 = fn.create_CNN_Resnet(0,img_shape[0],img_shape[1],depth,True)
+        cnn_1 = fn.create_CNN_Resnet(1,img_shape[0],img_shape[1],depth,True)
+        cnn_2 = fn.create_CNN_Resnet(2,img_shape[0],img_shape[1],depth,True)
+        x = Average()([cnn_0.output, cnn_1.output, cnn_2.output])
 
-now = datetime.now()
-dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-print('[INFO]: Training -', dt_string)
+        model_ft = Model(inputs=[cnn_0.input, cnn_1.input, cnn_2.input], outputs=x)
 
-train_df = pd.read_csv('./train.csv')
-validation_df = pd.read_csv('./validation.csv')
-test_df = pd.read_csv('./test.csv')
+        optimizer_ft = Adam(1e-5)
 
-train = fn.get_images_list(train_df,COLORMODE)
-validation = fn.get_images_list(validation_df,COLORMODE,weights=False)
-test = fn.get_images_list(test_df,COLORMODE,weights=False)
-test_not_augment = fn.get_images_list(test_df,COLORMODE,augment=False,weights=False)
-datagen = ImageDataGenerator()
-print("[INFO]: Starting image load process...")
-datagen_train = fn.image_generator(train, "./data_img_norm/", BATCH_SIZE, datagen, IMG_SHAPE, colormode=COLORMODE)
-print("[INFO]: Train data loaded...")
-datagen_val = fn.image_generator(validation, "./data_img_norm/", BATCH_SIZE, datagen, IMG_SHAPE, colormode=COLORMODE, weights=False)
-print("[INFO]: Validation data loaded...")
-datagen_test = fn.image_generator(test, "./data_img_norm/", 1, datagen, IMG_SHAPE, colormode=COLORMODE, shuffle=False, weights=False)
-datagen_test_not_augment = fn.image_generator(test_not_augment, "./data_img_norm/", 1, datagen, IMG_SHAPE, colormode=COLORMODE, shuffle=False, weights=False)
-print("[INFO]: Test data loaded...")
+        model_ft.compile(loss=MeanAbsoluteError(), optimizer=optimizer_ft, metrics=['mae'])
+        model_ft.summary()
 
+    ################################################################################
 
-early_stop_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=int(EPOCHS*0.2))
-model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpoint_filepath,
-    save_weights_only=True,
-    monitor='val_loss',
-    mode='min',
-    save_best_only=True,
-    verbose=VERBOSE)
+    print('Data:', color_mode)
+    print('Batch:', batch_size)
+    print('Epochs:', epochs)
+    print('Seed:', seed)
+    if resnet:
+        print('RESNET50')
+    else:
+        print('PANORAMA-CNN')
 
-history = model.fit(
-    datagen_train,
-    validation_data=datagen_val,
-    epochs=EPOCHS,
-    verbose=VERBOSE,
-    steps_per_epoch=(len(train) // BATCH_SIZE),
-    validation_steps=(len(validation) // BATCH_SIZE),
-    callbacks=[early_stop_callback, model_checkpoint_callback]
-)
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    print('[INFO]: Training -', dt_string)
 
-model.load_weights(checkpoint_filepath)
+    train_df = pd.read_csv(train_path)
+    validation_df = pd.read_csv(validation_path)
+    test_df = pd.read_csv(test_path)
 
-prediction = model.predict(datagen_test, verbose=VERBOSE, steps=len(test))
-true = np.array([float(n) for n in test[:,3]])
-pred = prediction.flatten()
+    train = fn.get_images_list(train_df,color_mode)
+    validation = fn.get_images_list(validation_df,color_mode,weights=False)
+    # test = fn.get_images_list(test_df,color_mode,weights=False)
+    test_not_augment = fn.get_images_list(test_df,color_mode,augment=False,weights=False)
+    datagen = ImageDataGenerator()
+    print("[INFO]: Starting image load process...")
+    datagen_train = fn.image_generator(train, data_path, batch_size, datagen, img_shape, colormode=color_mode)
+    print("[INFO]: Train data loaded...")
+    datagen_val = fn.image_generator(validation, data_path, batch_size, datagen, img_shape, colormode=color_mode, weights=False)
+    print("[INFO]: Validation data loaded...")
+    # datagen_test = fn.image_generator(test, data_path, 1, datagen, img_shape, colormode=color_mode, shuffle=False, weights=False)
+    datagen_test_not_augment = fn.image_generator(test_not_augment, data_path, 1, datagen, img_shape, colormode=color_mode, shuffle=False, weights=False)
+    print("[INFO]: Test data loaded...")
 
-fn.show_stats(true,pred)
+    if pretrained_model is not None:
+        model.load_weights(pretrained_model)
 
-mse.append(mean_squared_error(true, pred))
-r2.append(r2_score(true, pred)*100)
-rmse.append(mean_squared_error(true, pred, squared= False))
-mae.append(mean_absolute_error(true, pred))
-
-prediction_not_augment = model.predict(datagen_test_not_augment, verbose=VERBOSE, steps=len(test_not_augment))
-true = np.array([float(n) for n in test_not_augment[:,3]])
-pred = prediction_not_augment.flatten()
-
-fn.show_stats(true,pred)
-
-mse.append(mean_squared_error(true, pred))
-r2.append(r2_score(true, pred)*100)
-rmse.append(mean_squared_error(true, pred, squared= False))
-mae.append(mean_absolute_error(true, pred))
-
-
-kfold_stats_df = pd.DataFrame()
-kfold_stats_df['MSE'] = mse
-kfold_stats_df['RMSE'] = rmse
-kfold_stats_df['MAE'] = mae
-kfold_stats_df['R2'] = r2
-
-print(kfold_stats_df.to_string())
-
-if RESNET:
-    print('FINE TUNNING')
-    model_ft.load_weights(checkpoint_filepath)
-
-    early_stop_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=int(EPOCHS*0.2))
+    early_stop_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=int(epochs*0.2))
     model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_filepath,
         save_weights_only=True,
         monitor='val_loss',
         mode='min',
         save_best_only=True,
-        verbose=VERBOSE)
+        verbose=verbose_level)
 
-    history = model_ft.fit(
+    history = model.fit(
         datagen_train,
         validation_data=datagen_val,
-        epochs=EPOCHS,
-        verbose=VERBOSE,
-        steps_per_epoch=(len(train) // BATCH_SIZE),
-        validation_steps=(len(validation) // BATCH_SIZE),
+        epochs=epochs,
+        verbose=verbose_level,
+        steps_per_epoch=(len(train) // batch_size),
+        validation_steps=(len(validation) // batch_size),
         callbacks=[early_stop_callback, model_checkpoint_callback]
     )
 
-    model_ft.load_weights(checkpoint_filepath)
+    history_df = pd.DataFrame.from_dict(history.history)
 
-    prediction = model_ft.predict(datagen_test, verbose=VERBOSE, steps=len(test))
-    true = np.array([float(n) for n in test[:,3]])
-    pred = prediction.flatten()
+    model.load_weights(checkpoint_filepath)
 
-    fn.show_stats(true,pred)
+    # prediction = model.predict(datagen_test, verbose=verbose_level, steps=len(test))
+    # true = np.array([float(n) for n in test[:,3]])
+    # pred = prediction.flatten()
 
-    mse.append(mean_squared_error(true, pred))
-    r2.append(r2_score(true, pred)*100)
-    rmse.append(mean_squared_error(true, pred, squared= False))
-    mae.append(mean_absolute_error(true, pred))
+    # df_stats_augment, df_precision_augment = fn.show_stats(true,pred)
 
-    prediction_not_augment = model_ft.predict(datagen_test_not_augment, verbose=VERBOSE, steps=len(test_not_augment))
+    # name.append("Prediction Augment")
+    # mse.append(mean_squared_error(true, pred))
+    # r2.append(r2_score(true, pred))
+    # rmse.append(mean_squared_error(true, pred, squared= False))
+    # mae.append(mean_absolute_error(true, pred))
+
+    prediction_not_augment = model.predict(datagen_test_not_augment, verbose=verbose_level, steps=len(test_not_augment))
     true = np.array([float(n) for n in test_not_augment[:,3]])
     pred = prediction_not_augment.flatten()
 
-    fn.show_stats(true,pred)
+    df_stats_not_augment, df_precision_not_augment = fn.show_stats(true,pred)
 
+    name.append("Prediction NOT Augment")
     mse.append(mean_squared_error(true, pred))
-    r2.append(r2_score(true, pred)*100)
+    r2.append(r2_score(true, pred))
     rmse.append(mean_squared_error(true, pred, squared= False))
     mae.append(mean_absolute_error(true, pred))
 
+
     kfold_stats_df = pd.DataFrame()
+    kfold_stats_df['Name'] = name
     kfold_stats_df['MSE'] = mse
     kfold_stats_df['RMSE'] = rmse
     kfold_stats_df['MAE'] = mae
     kfold_stats_df['R2'] = r2
 
     print(kfold_stats_df.to_string())
-    model_ft.save_weights(OUTPUT+'/model_rgb_resnet/model_rgb_resnet')
 
-else:
-    if COLORMODE == 'rgb':
-        folder = OUTPUT + '/model_rgb_panoramacnn/model_rgb_panoramacnn'
-    elif COLORMODE == 'grayscale':
-        folder = OUTPUT + '/model_gray_panoramacnn/model_gray_panoramacnn'
-    model.save_weights(folder)
+    if not resnet:
+        if color_mode == 'rgb':
+            output_subfolder = 'model_rgb_panoramacnn'
+            output_folder = os.path.join(output, output_subfolder)
+            os.makedirs(output_folder, exist_ok=True)
+            output_path = os.path.join(output, output_subfolder, 'model_rgb_panoramacnn')
+            model.save_weights(output_path)
 
-print('################################################################################')
-mse.append(np.mean(mse))
-r2.append(np.mean(r2))
-rmse.append(np.mean(rmse))
-mae.append(np.mean(mae))
+        elif color_mode == 'grayscale':
+            output_subfolder = 'model_gray_panoramacnn'
+            output_folder = os.path.join(output, output_subfolder)
+            os.makedirs(output_folder, exist_ok=True)
+            output_path = os.path.join(output, output_subfolder, 'model_gray_panoramacnn')
 
-kfold_stats_df = pd.DataFrame()
-kfold_stats_df['MSE'] = mse
-kfold_stats_df['RMSE'] = rmse
-kfold_stats_df['MAE'] = mae
-kfold_stats_df['R2'] = r2
+        history_df.to_csv(os.path.join(output_folder, "history.csv"), sep = ";")
+        # df_stats_augment.to_csv(os.path.join(output_folder, "stats_augment.csv"), sep = ";")
+        # df_precision_augment.to_csv(os.path.join(output_folder, "precision_augment.csv"), sep = ";")
+        df_stats_not_augment.to_csv(os.path.join(output_folder, "stats_not_augment.csv"), sep = ";")
+        df_precision_not_augment.to_csv(os.path.join(output_folder, "precision_not_augment.csv"), sep = ";")
 
-print('\n')
-print('Data:', COLORMODE)
-print('Batch:', BATCH_SIZE)
-print('Epochs:', EPOCHS)
-print('Seed:', SEED)
-if RESNET:
-    print('RESNET50')
-else:
-    print('PANORAMA-CNN')
+    else:
+        output_subfolder = "model_rgb_resnet"
+        output_folder = os.path.join(output, output_subfolder)
+        os.makedirs(output_folder, exist_ok=True)
+        output_path = os.path.join(output_folder, 'model_rgb_resnet')
+        model.save_weights(output_path)
+        history_df.to_csv(os.path.join(output_folder, "history.csv"), sep = ";")
+        df_stats_not_augment.to_csv(os.path.join(output_folder, "stats_not_augment.csv"), sep = ";")
+        df_precision_not_augment.to_csv(os.path.join(output_folder, "precision_not_augment.csv"), sep = ";")
 
-now = datetime.now()
-dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-print(dt_string)
+        print('FINE TUNNING')
 
-print(kfold_stats_df.to_string())
+        model_ft.load_weights(checkpoint_filepath)
+
+        early_stop_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=int(epochs*0.2))
+        model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+            filepath=checkpoint_filepath,
+            save_weights_only=True,
+            monitor='val_loss',
+            mode='min',
+            save_best_only=True,
+            verbose=verbose_level)
+
+        history = model_ft.fit(
+            datagen_train,
+            validation_data=datagen_val,
+            epochs=epochs,
+            verbose=verbose_level,
+            steps_per_epoch=(len(train) // batch_size),
+            validation_steps=(len(validation) // batch_size),
+            callbacks=[early_stop_callback, model_checkpoint_callback]
+        )
+
+        history_df_ft = pd.DataFrame.from_dict(history.history)
+
+        model_ft.load_weights(checkpoint_filepath)
+
+        # prediction = model_ft.predict(datagen_test, verbose=verbose_level, steps=len(test))
+        # true = np.array([float(n) for n in test[:,3]])
+        # pred = prediction.flatten()
+
+        # df_stats_augment_ft, df_precision_augment_ft = fn.show_stats(true,pred)
+
+        # name.append("Prediction Augment FT")
+        # mse.append(mean_squared_error(true, pred))
+        # r2.append(r2_score(true, pred))
+        # rmse.append(mean_squared_error(true, pred, squared= False))
+        # mae.append(mean_absolute_error(true, pred))
+
+        prediction_not_augment = model_ft.predict(datagen_test_not_augment, verbose=verbose_level, steps=len(test_not_augment))
+        true = np.array([float(n) for n in test_not_augment[:,3]])
+        pred = prediction_not_augment.flatten()
+
+        df_stats_not_augment_ft, df_precision_not_augment_ft = fn.show_stats(true,pred)
+
+        name.append("Prediction NOT Augment FT")
+        mse.append(mean_squared_error(true, pred))
+        r2.append(r2_score(true, pred))
+        rmse.append(mean_squared_error(true, pred, squared= False))
+        mae.append(mean_absolute_error(true, pred))
+
+        kfold_stats_df = pd.DataFrame()
+        kfold_stats_df['Name'] = name
+        kfold_stats_df['MSE'] = mse
+        kfold_stats_df['RMSE'] = rmse
+        kfold_stats_df['MAE'] = mae
+        kfold_stats_df['R2'] = r2
+
+        print(kfold_stats_df.to_string())
+        output_subfolder = "model_rgb_resnet_ft"
+        output_folder = os.path.join(output, output_subfolder)
+        os.makedirs(output_folder, exist_ok=True)
+        output_path = os.path.join(output_folder, 'model_rgb_resnet_ft')
+        model_ft.save_weights(output_path)
+
+        history_df_ft.to_csv(os.path.join(output_folder, "history_ft.csv"), sep = ";")
+        df_stats_not_augment_ft.to_csv(os.path.join(output_folder, "stats_not_augment_FT.csv"), sep = ";")
+        df_precision_not_augment_ft.to_csv(os.path.join(output_folder, "precision_not_augment_FT.csv"), sep = ";")
+
+    print('################################################################################')
+
+    kfold_stats_df = pd.DataFrame()
+    kfold_stats_df['Name'] = name
+    kfold_stats_df['MSE'] = mse
+    kfold_stats_df['RMSE'] = rmse
+    kfold_stats_df['MAE'] = mae
+    kfold_stats_df['R2'] = r2
+
+    print('\n')
+    print('Data:', color_mode)
+    print('Batch:', batch_size)
+    print('Epochs:', epochs)
+    print('Seed:', seed)
+    if resnet:
+        result_code = "RESNET50"
+    else:
+        result_code = "PANORAMA-CNN"
+
+    print(result_code)
+
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    print(dt_string)
+
+    print(kfold_stats_df.to_string())
+    kfold_stats_df.to_csv(os.path.join(output, "execution_results_{}.csv".format(result_code)), sep=";")
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-p",
+        "--parameters",
+        help="JSON file with parameters configuration",
+        type=str,
+        required=True,
+    )
+
+    args = parser.parse_args()
+
+    json_file = open(args.parameters, "r")
+    args = json.load(json_file)
+    json_file.close()
+
+    if args["color_mode"] == 'rgb':
+        img_shape = (36,108)
+        depth = 3
+        if args["resnet"]:
+            cnn = fn.create_CNN_Resnet
+        else:
+            cnn = fn.create_CNN
+    elif args["color_mode"] == 'grayscale':
+        img_shape = (108,108)
+        depth = 1
+        cnn = fn.create_CNN
+    else:
+        raise RuntimeError("Color mode option unknown")
+
+    random.seed(args["seed"])
+    np.random.seed(args["seed"])
+    tf.random.set_seed(args["seed"])
+
+    log_level = logging.WARNING
+    if args["verbose"] == 0:
+        log_level = logging.WARNING
+    elif args["verbose"] == 1:
+        log_level = logging.INFO
+    elif args["verbose"] == 2:
+        log_level = logging.DEBUG
+    else:
+        logging.warning('Log level not recognised. Using WARNING as default')
+
+    logging.getLogger().setLevel(log_level)
+
+    logging.warning("Verbose level set to {}".format(logging.root.level))
+
+    main(
+        args["train"],
+        args["val"],
+        args["test"],
+        args["data"],
+        args["checkpoint"],
+        args["batch_size"], 
+        args["color_mode"], 
+        args["epochs"], 
+        args["output"], 
+        args["resnet"], 
+        args["seed"], 
+        img_shape, 
+        depth, 
+        cnn,
+        args["verbose_keras"],
+        args["pretrained_model"])
